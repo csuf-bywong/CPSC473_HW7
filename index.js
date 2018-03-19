0e94bd51e5677c9193ddae8a712473101992ccfe
Belinda <bywong@csu.fullerton.edu>
2018-03-18 17:42:12 -0700

    completed bronze challenge: creating a custom error page

---
+++ index.js
@@ -0,0 +1,26 @@
+/*eslint-disable no-undef*/
+var http = require("http");
+var fs = require("fs");
+var extract = require("./extract");
+
+var handleError = function (err, res) {
+  res.writeHead(404); // write eror to browser
+  fs.readFile("app/error.html", function(err, data) {
+    res.end(data);
+  });
+};
+
+var server = http.createServer(function(req, res) {
+  console.log("Responding to a request.");
+
+  var filePath = extract(req.url);
+  fs.readFile(filePath, function(err, data) {
+    if (err) {
+      handleError(err, res);
+      return;
+    } else {
+      res.end(data);
+    }
+  });
+});
+server.listen(3000);
